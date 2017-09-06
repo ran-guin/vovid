1b2a7c4eaedc313c34dc1cd3dfd97a647115a001
Ran Guin <ran.guin@gmail.com>
2017-09-05 19:10:18 -0700

    update urls

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   links: ['Visit', 'User', 'Stats'],
 
   vaccineURL: 'https://vaccineURL',
-  userURL: 'http://localhost:1234/Record_API/search?table=user',
-  dbURL: 'http://localhost:1234/Record_API/fields',
-  apiUrl: 'http://localhost:3001/'
+  userURL: 'https://litmus-a1.herokuapp.com/Record_API/search?table=user',
+  dbURL: 'https://litmus-a1.herokuapp.com/Record_API/fields',
+  apiUrl: 'https://test-api.herokuapp.com'
 }
